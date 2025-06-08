ad987ee212c5a3243ca199c33e7f9b2c410d2093
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-08 05:46:29 +0300

    Update views.py

---
+++ soma/backend/comments/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .models import Comment
+from .serializers import CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
