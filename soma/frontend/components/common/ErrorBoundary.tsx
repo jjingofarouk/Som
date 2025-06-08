b6c35e29fe77292c4318bbabb5a67d227f755ebf
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-08 06:10:17 +0300

    Update ErrorBoundary.tsx

---
+++ soma/frontend/components/common/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Component, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+  state: State = {
+    hasError: false,
+  };
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback || <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
