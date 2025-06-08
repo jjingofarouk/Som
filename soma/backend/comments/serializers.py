720220e0c33f602a732dbc28ccdcab07c4ba4914
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-08 05:46:14 +0300

    Update serializers.py

---
+++ soma/backend/comments/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Comment
+from users.serializers import UserSerializer
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'user', 'lesson', 'created_at', 'updated_at']
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
